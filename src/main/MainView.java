dd37fe27f956bcbdbeb51e0713ec3bc185faf791
Frank Wang <fraankwang@gmail.com>
2016-02-23 12:02:37 -0500

    added command reading input that calls runCommand in MainModel

--- src/main/MainView.java
+++ src/main/MainView.java
@@ -9,14 +9,19 @@ import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import constants.Constants;
 
 public class MainView {
+	
+	private final int SCENE_WIDTH = 800;
+	private final int SCENE_HEIGHT = 800;
 	
 	private Stage myPrimaryStage;
 	private MainController myController;
@@ -35,7 +40,7 @@ public class MainView {
 	}
 
 	public void display () {
-		Scene scene = new Scene(myPrimaryRoot, 500, 500, Color.WHITE);
+		Scene scene = new Scene(myPrimaryRoot, SCENE_WIDTH, SCENE_HEIGHT, Color.WHITE);
 		myPrimaryStage.setScene(scene);
 		myPrimaryStage.show();
 		
@@ -49,19 +54,27 @@ public class MainView {
 		//createPaneElements and place them in myPrimaryPane
 		
 		
-							
-		TextInputDialog inputBox = createInputBox();
-		//can't add TextInputDialog to Pane??
+		//TextField needs to be wrapped in some UI thing that can be aligned around					
+		TextField tf = makeTextField();
+		myPrimaryPane.getChildren().add(tf);
 		
 		myPrimaryRoot = root;
 	}
 	
 	
-	
-	private TextInputDialog createInputBox () {
-		TextInputDialog inputBox = new TextInputDialog();
+	private TextField makeTextField () {
+		TextField textField = new TextField(Constants.getSpecification("TextFieldDefault"));
 		
-		return inputBox;
+		textField.setOnAction(new EventHandler<ActionEvent>() {
+            @Override      
+            public void handle (ActionEvent event) {       
+                myController.runCommand(textField.getText());
+                textField.clear();
+            }      
+        });
+		
+		return textField;
+		
 	}
 	
 	private void placePaneElement (Node element, String position) {
--- src/model/MainModel.java
+++ src/model/MainModel.java
@@ -7,7 +7,10 @@ package model;
 import main.MainController;
 
 public class MainModel {
-	private MainController myController;
+	private MainController myController; 
+	// we can eliminate a dependency if we have getters in the 
+	// MainModel class (i.e. getHistory) that calls respective Model elements to return their values
+	// I think the MainModel should also do the translating then
 	private CommandParser myParser;
 	private History myHistory;
 	private TurtlePlayground myPlayground;
@@ -19,6 +22,11 @@ public class MainModel {
 	
 	public void readCommand (String input) {
 		// wrapper method that does everything (extensive use of helper classes and objects)
+		// parse command
+		// calculate/run the command
+		// store command in history and update history
+		// anything else I missed
+		System.out.println("command read: " + input);
 	}
 	
 	public CommandParser getMyParser() {
