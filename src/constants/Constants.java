967ea6d0507e004ca199f08147d86fde02b724d8
HUIJIA YU <hy91@duke.edu>
2016-02-23 18:14:42 -0500

    Revert "Revert "add constants for actions""
    This reverts commit 15ba1f7f6f02f2bdfdf7d1708a9e890b0098bb01.

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -7,21 +7,28 @@ package constants;
 import java.util.ResourceBundle;
 
 public class Constants {
-	
+
 	private static final String DEFAULT_RESOURCE_PACKAGE = "resources/";
-    private static final String DEFAULT_RESOURCE_FILE = "Specifications";
-    private static final String DEFAULT_LANGUAGE = "languages/English";
-    private static final ResourceBundle SPECIFICATIONS =
-            ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + DEFAULT_RESOURCE_FILE);
-    private static final ResourceBundle COMMANDS =
-    		ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + DEFAULT_LANGUAGE);
-    
-    public static String getSpecification (String name) {
-    	return SPECIFICATIONS.getString(name);
-    }
-    
-    public static String getCommand (String command) {
-    	return COMMANDS.getString(command);
-    }
-    
+	private static final String DEFAULT_RESOURCE_FILE = "Specifications";
+	private static final String DEFAULT_LANGUAGE = "languages/English";
+	public static final String DEFAULT_ACTIONS = "possibleactions";
+
+	private static final ResourceBundle SPECIFICATIONS = ResourceBundle
+			.getBundle(DEFAULT_RESOURCE_PACKAGE + DEFAULT_RESOURCE_FILE);
+	private static final ResourceBundle COMMANDS = ResourceBundle
+			.getBundle(DEFAULT_RESOURCE_PACKAGE + DEFAULT_LANGUAGE);
+	private static final ResourceBundle ACTIONS = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + DEFAULT_ACTIONS);
+
+	public static String getSpecification(String name) {
+		return SPECIFICATIONS.getString(name);
+	}
+
+	public static String getCommand(String command) {
+		return COMMANDS.getString(command);
+	}
+
+	public static String getAction(String action) {
+		return ACTIONS.getString(action);
+	}
+
 }
--- src/model/CommandParser.java
+++ src/model/CommandParser.java
@@ -6,39 +6,36 @@ package model;
 
 import java.lang.reflect.*;
 import java.util.*;
+
+import constants.Constants;
 import model.action.*;
 
 public class CommandParser {
-	Collection<String> myActions;
-	Stack<String> stack;
-	public CommandParser(){
-				
+	private Stack<String> stack;
+	public CommandParser() {
+
 	}
-	
-	public void parse (String input) {
+
+	public void parse(String input) {
 		Stack<String> stack = new Stack<String>();
 		stack.addAll(Arrays.asList(input.split(" ")));
-	
+
 	}
-	
-	public void c(){
-		
-	}
-	
+
 	public double checkNext() throws Exception {
-		String next  = stack.pop();
-		if(myActions.contains(next)){
-			Class a = Class.forName(next);
+		String next = stack.pop();
+		if (actionResources.containsKey(next)) {
+			//??????? also language?
+			Class a = Class.forName(Constants.getAction(next));
 			Constructor constructor = a.getConstructors()[0];
-			double[] params = new double [constructor.getParameterTypes().length];
-			for (int i = 0; i < params.length; i++){
+			double[] params = new double[constructor.getParameterTypes().length];
+			for (int i = 0; i < params.length; i++) {
 				params[i] = checkNext();
 			}
-			
+
 			Action action = (Action) constructor.newInstance(params);
 			return action.rule();
-		}
-		else{
+		} else {
 			return Double.parseDouble(next);
 		}
 	}
