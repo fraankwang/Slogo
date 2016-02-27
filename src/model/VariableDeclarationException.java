f7136f434c4e51a8c96321af8aa8458f14c969b2
HUIJIA YU <hy91@duke.edu>
2016-02-26 19:22:08 -0500

    hierarchy

---
+++ src/model/VariableDeclarationException.java
@@ -0,0 +1,30 @@
+package model;
+
+public class VariableDeclarationException extends Exception {
+
+	public VariableDeclarationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public VariableDeclarationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public VariableDeclarationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public VariableDeclarationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public VariableDeclarationException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/model/action/Math/Math.java
@@ -0,0 +1,14 @@
+package model.action.Math;
+
+import model.TurtlePlayground;
+import model.Variables;
+import model.action.Action;
+
+public abstract class MathTwoParams extends Action {
+
+	public MathTwoParams (Variables variables) {
+		super(variables);
+	
+	}
+
+}
