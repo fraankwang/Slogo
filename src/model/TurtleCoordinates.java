64a9a9f739ee9943e70eaeaa6ca64fb32e7ccfac
srikarpyda <sp285@duke.edu>
2016-02-27 19:51:44 -0500

    updated TurtleCoordiantes

---
+++ src/model/TurtleCoordinates.java
@@ -0,0 +1,12 @@
+package model;
+
+public class TurtleCoordinates {
+
+	Double xCoord;
+	Double yCoord;
+	
+	public TurtleCoordinates(Double xCoordinate, Double yCoordinate){
+		xCoord=xCoordinate;
+		yCoord=yCoordinate;
+	}
+}
