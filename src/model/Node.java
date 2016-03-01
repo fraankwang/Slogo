d31f31fa243dcc76606cc8391bff456b34b89552
HUIJIA YU <hy91@duke.edu>
2016-03-01 15:04:41 -0500

    fixed bracket parsing

--- src/model/Node.java
+++ src/model/Node.java
@@ -12,20 +12,20 @@ public class Node {
 	public Node(String dataString, List<Node> childList){
 		data = dataString;
 		children = childList;
-		
+
 	}
 	public Node(){
 		data=new String();
 		children=new ArrayList<Node>();
 	}
-	
+
 	public Node makeCommandString(Queue<String> queue, Node tree){
 		queue.poll();
-		StringBuilder commandstring = new StringBuilder(" ");
+		StringBuilder commandstring = new StringBuilder();
 		int opencount = 1;
 		int closecount = 0;
 		while (true) {
-			
+
 			if(isOpenBracket(queue)){
 				opencount++;
 			}
@@ -39,43 +39,44 @@ public class Node {
 			commandstring.append(action + " ");
 		}
 		queue.poll();
-		
-		tree.data =commandstring.deleteCharAt(commandstring.length() - 1).toString();
+		if(commandstring.length()!=0){
+			tree.data =commandstring.deleteCharAt(commandstring.length() - 1).toString();
+		}
 		return tree;
 	}
-	
-	
+
+
 	public String getData(){
 		return this.data;
 	}
-	
+
 	public List<Node> getChildren(){
 		return this.children;
 	}
-	
+
 	public void setData(String data){
 		this.data=data;
 	}
-	
+
 	public void setChildren(List<Node> children){
 		this.children=children;
 	}
-	
+
 	public  boolean isOpenBracket(Queue<String> queue){
 		return queue.peek().equals(Constants.OPEN_BRACKET);
 	}
 	public  boolean isCloseBracket(Queue<String> queue){
 		return queue.peek().equals(Constants.CLOSE_BRACKET);
 	}
-	
+
 	public void addChild(Node node){
 		getChildren().add(node);
 	}
-	
+
 	public boolean areChildrenEmpty(){
 		return getChildren().isEmpty();
 	}
-	
+
 	public boolean isVariable(){
 		System.out.println("  variable!");
 		return getData().startsWith(":");
